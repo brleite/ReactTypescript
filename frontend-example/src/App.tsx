5bea9537fd60bd23278e7c42d5387587791e8e3e
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-22 16:01:37 -0300

    Primeiras rotas

--- frontend-example/src/App.tsx
+++ frontend-example/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() /* : JSX.Element */ {
+import Routes from './routes';
+
+/* function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
   );
-}
+} */
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
