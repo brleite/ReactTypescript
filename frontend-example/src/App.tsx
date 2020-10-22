db50720bc6277827eb8bc78c2503c27f6ed2299e
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-22 16:32:18 -0300

    Styled components

--- frontend-example/src/App.tsx
+++ frontend-example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 /* function App() {
@@ -12,9 +12,12 @@ import Routes from './routes';
 } */
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
