302494fbe42707dabc978c22b0e6c6a24849e91b
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-27 09:54:37 -0300

    Animação dos toasts

--- frontend/src/hooks/toast.tsx
+++ frontend/src/hooks/toast.tsx
@@ -1,26 +1,57 @@
-import React, { createContext, useContext, useCallback } from 'react';
+import React, { createContext, useContext, useCallback, useState } from 'react';
+import { uuid } from 'uuidv4';
 
 import ToastContainer from '../components/toastcontainer';
 
+export interface ToastMessage {
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description?: string;
+}
+
 interface ToastContextData {
-  addToast(): void;
-  removeToast(): void;
+  addToast(message: Omit<ToastMessage, 'id'>): void;
+  removeToast(is: string): void;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 
 const ToastProvider: React.FC = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('addToast');
-  }, []);
-  const removeToast = useCallback(() => {
-    console.log('removeToast');
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
+
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+      const id = uuid();
+
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+
+      /**
+       * Dessa forma, precisaria colocar messages no array de dependências
+       */
+      // setMessages([...messages, toast]);
+
+      /**
+       * Dessa forma, não precisa colocar messages no array de dependéncias.
+       * Sempre que se usa um set, podemos passar uma function que receve como parâmetro o valor anterior.
+       */
+      setMessages(oldMessages => [...oldMessages, toast]);
+    },
+    [],
+  );
+  const removeToast = useCallback((id: string) => {
+    setMessages(state => state.filter(message => message.id !== id));
   }, []);
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={messages} />
     </ToastContext.Provider>
   );
 };
