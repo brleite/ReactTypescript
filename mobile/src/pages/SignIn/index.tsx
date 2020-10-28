4ded795f2aeed1636bb088e7d076b77f5d6a8736
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-28 10:36:12 -0300

    Ajustes no login

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 // Não é preciso especificar o tamanho. Ele será automaticamente resolvido de acordo com a densidade de pixels do dispositivo
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -12,6 +15,17 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
