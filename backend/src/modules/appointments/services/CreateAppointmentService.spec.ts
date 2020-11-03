0c2d78cd0327b629ac50e4223ce8d106c663fd4c
Bruno Mayerle Leite <brleite@gmail.com>
2020-11-03 16:03:14 -0300

    Configuração do jest com fake repository

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,10 +1,23 @@
+import 'reflect-metadata';
 import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
 // Agrupa testes por categoria
 describe('CreateAppointment', () => {
   // Alternativa ao uso do test();
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12234234',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12234234');
   });
 
   /* it('should not be able to create two appointments on the same time', () => {
