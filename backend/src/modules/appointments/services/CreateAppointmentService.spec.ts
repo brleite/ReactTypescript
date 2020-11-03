1f7f0bd5c81d269a346e2fb1c490c63f7ce48029
Bruno Mayerle Leite <brleite@gmail.com>
2020-11-03 15:21:24 -0300

    Início da criação dos testes

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,13 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+// Agrupa testes por categoria
+describe('CreateAppointment', () => {
+  // Alternativa ao uso do test();
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  /* it('should not be able to create two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  }); */
 });
