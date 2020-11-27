27390eb87df9b3efb3a9d37de0ecc58300db13ae
Bruno Mayerle Leite <brleite@gmail.com>
2020-11-27 10:17:52 -0300

    Configuração para utilizar o storage S3 da AWS

--- backend/src/modules/users/infra/typeorm/entities/User.ts
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -1,4 +1,6 @@
 import { Exclude, Expose } from 'class-transformer';
+import uploadConfig from '@config/upload';
+
 import {
   Entity,
   Column,
@@ -33,9 +35,20 @@ class User {
 
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return this.avatar
+          ? `${process.env.APP_API_URL}/files/${this.avatar}`
+          : null;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
