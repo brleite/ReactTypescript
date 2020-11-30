a6a239cd0cd5095661b430e9ddd051c92fdb300b
Bruno Mayerle Leite <brleite@gmail.com>
2020-11-30 16:07:21 -0300

    Cache de appointments

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
+// import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 /* import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'; */
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -12,7 +12,8 @@ export default class AppointmentsController {
 
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+    // Não é mais necessário pois está sendo convertido para Joi.date na validação
+    // const parsedDate = parseISO(date);
 
     /* const createAppointment = new CreateAppointmentService(
     appointmentsRepository,
@@ -21,7 +22,8 @@ export default class AppointmentsController {
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
-      date: parsedDate,
+      // date: parsedDate,
+      date,
       provider_id,
       user_id,
     });
